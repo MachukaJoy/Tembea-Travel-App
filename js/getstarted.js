1ed7e335567154a10a6541a739cc1c22b0048774
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-24 23:16:50 +0300

    pt:form - fixes user selection and updates user suggestions

--- js/getstarted.js
+++ js/getstarted.js
@@ -1,106 +1,3 @@
-<<<<<<< HEAD
-// var submit = document.getElementById("submit")
-// var budget = document.getElementById("budget")
-// var destination = document.getElementById("destination")
-
-// submit.addEventListener("click", function (e) {
-// e.preventDefault()
-// destinationFinal()
-// })
-
-// let destinationBeach = {
-// low: " the beautiful kilifi",
-// midlow: " Malindi the hidden gem !!",
-// midhi: " the amazing Diani !!",
-// high: " the majestic maldives !!"
-// }
-
-// let destinationMount = {
-// low: "the cool and amazing Ngong Hills!!",
-// midlow: " the great Mt Longonot !!",
-// midhi: " the beautiful Fairmont MountKenya!!",
-// high: " the coveted Swiss Alps!!"
-// }
-
-// let destinationWild = {
-
-// low: "Nairobi National park",
-// midlow: " the mighty amboseli",
-// midhi: " the coveted Masai Mara",
-// high: "the amazing Serengeti national park"
-// }
-
-
-// function destinationCheck() {
-// if (budget.value === "low" && destination.value === "beach") {
-// return destinationBeach.low
-
-// } else if (budget.value === "midlow" && destination.value === "beach") {
-// return destinationBeach.midlow
-// } else if (budget.value === "midhi" && destination.value === "beach") {
-// return destinationBeach.midhi
-// } else if (budget.value === "high" && destination.value === "beach") {
-// return destinationBeach.high
-// } else if (budget.value === "low" && destination.value === "mountain") {
-// return destinationMount.low
-// } else if (budget.value === "midlow" && destination.value === "mountain") {
-// return destinationMount.midlow
-// } else if (budget.value === "midhi" && destination.value === "mountain") {
-// return destinationMount.midhi
-// } else if (budget.value === "high" && destination.value === "mountain") {
-// return destinationMount.high
-// }else if (budget.value === "low" && destination.value === "wildlife") {
-// return destinationWild.low
-// } else if (budget.value === "midlow" && destination.value === "wildlife") {
-// return destinationWild.midlow
-// } else if (budget.value === "midhi" && destination.value === "wildlife") {
-// return destinationWild.midhi
-// } else if (budget.value === "high" && destination.value === "wildlife") {
-// return destinationWild.high
-// }
-
-// }
-
-// function destinationFinal() {
-
-// document.getElementById("destination-display").innerHTML = "congtatulations!!, your destination is" +' '+ (destinationCheck())
-
-// }
-
-var submit = document.getElementById("submit")
-var budget = document.getElementById("budget")
-var destination = document.getElementById("destination")
-var active = document.getElementsByClassName('travel-select')
-
-submit.addEventListener("click", function (e) {
-     e.preventDefault()
-     checkDestination()
-    });
-
-let price = {
-    local : ["Mount-Kenya", "Mount-Longonot", "Mombasa", "Lamu", "Turkana", "Watamu", "Kisumu"],
-    international : ["Dubai", "Hawaii", "Sweden"]
-};
-
-let activitiesUnderTaken = {
-    active : ['Kayaking','yatch ride','cruise dinner','scuba diving','jetskiing','swimming with dolphins',
-    'meditation','swimming','waterp polo','snokerling','beach walks','star gazing'],
-    inActive:['Nature trails','tag of war','bird watching', 'bike riding', 'camping','bonfire','horse riding','fishing', 'ziplinig']
-
-    
-}
-let budgetOfPerson = {
-    low : "10,000",
-    medium : "30,000",
-    high : "100,000"
-}
-
-function checkDestination (){
-    if (budget.value == low && active.value == active){
-        alert(activitiesUnderTaken.active +'' + price.local);
-    }
-}
-=======
 var submit = document.getElementById("submit");
 var budget = document.getElementById("budget");
 var destination = document.getElementById("destination");
@@ -115,22 +12,22 @@ submit.addEventListener("click", function (e) {
 });
 
 let destinationBeach = {
-  low: [" the beautiful kilifi", "the tamu watamu"," the mtwapa creek"],
-  mid: [" the amazing Diani !!", "Lamu tamu"],
-  high: [" the majestic maldives !!", " and Hawai","the beautiful Brazil", "the delight of Dubai"],
+  low: [" The beautiful kilifi", "The tamu watamu"," The Mtwapa creek"],
+  mid: [" The amazing Diani !!", "Lamu tamu"],
+  high: [" The majestic maldives", " and Hawai","the beautiful Brazil", "the delight of Dubai"],
 };
 
 let destinationMount = {
-  low: "the cool and amazing Ngong Hills!!",
-  mid: " the beautiful Fairmont MountKenya!!",
-  high: " the coveted Swiss Alps!!",
+  low: "The cool and amazing Ngong Hills!!",
+  mid: " The beautiful Fairmont MountKenya!!",
+  high: " The coveted Swiss Alps!!",
 };
 
 let destinationWild = {
   low: ["Nairobi National park","lake Nakuru National park"],
 
-  mid: ["the coveted Masai Mara", "the great Amboseli"],
-  high: ["the amazing Serengeti national park", 'Kruger National park'],
+  mid: ["The coveted Masai Mara", "the great Amboseli"],
+  high: ["The amazing Serengeti national park", 'Kruger National park'],
 };
 
 function destinationCheck() {
@@ -157,8 +54,8 @@ function destinationCheck() {
 
 function destinationFinal() {
   document.getElementById("destination-display").innerHTML =
-    "congtatulations!!, choose a destination out of" +
-    " " +
+    "Congratulations!!, You can choose a destination out of" +
+    " " + 
     destinationCheck() 
     document.getElementById("activities-display").innerHTML =
 
@@ -219,8 +116,7 @@ mountGrouplow = ["Nature trails", "tag of war", "bird watching", "ziplining"];
 
 var wildSoloHigh = ["Hot air baloon", "dinner by the bush"];
 var wildSoloMedium = ["Game drive", "cultural visit", "cultural dances"];
-var wildSoloLow = ["opharnage", "nature walk", "bird watching"];
->>>>>>> fa45d1d509f63dcd0f1d8b6a1f51cc5c4bac6518
+var wildSoloLow = ["orphanage", "nature walk", "bird watching"];
 
 function activityCheck() {
   if (budget.value === "high" && destination.value === "beach") {
